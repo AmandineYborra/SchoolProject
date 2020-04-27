9f1670083a43efb3e3808afd34a7bfb3495cce35
AmandineYborra <44371951+AmandineYborra@users.noreply.github.com>
2020-04-27 19:19:35 +0200

    Add files via upload

--- webapp/app.py
+++ webapp/app.py
@@ -29,4 +29,4 @@ def predict():
     return render_template("index.html", prediction_text = 'Le bien est estimé à {} €'.format(output))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8080)
